bcbcd6d1eb44f9a1a96d9871b72eb29c8f0ebe76
rolandobrown <rolando.brown@gmail.com>
2015-08-14 11:44:16 -0400

    add knowledge of binary search tree tool

--- agreements.rb
+++ agreements.rb
@@ -1,25 +1,25 @@
 ## AGREEMENTS
 
 class Human
-  attr_reader :allies, :connected
+  attr_reader :allies, :connected  # => nil
 
   def initialize(initial_allies = [])   #store instances of alliances
-    @allies = []
-    initial_allies.each do |ally|
-      form_alliance_with(ally)
-    end
+    @allies = []                   # => [], [], []
+    initial_allies.each do |ally|  # => [], [#<Human:0x007fd0b3823bc0 @allies=[]>], []
+      form_alliance_with(ally)     # => nil
+    end                            # => [], [#<Human:0x007fd0b3823bc0 @allies=[#<Human:0x007fd0b3823760 @allies=[#<Human:0x007fd0b3823bc0 ...>]>]>], []
   end
 
   def form_alliance_with(human)   # define methods
-    @allies << human
+    @allies << human                       # => [#<Human:0x007fd0b3823bc0 @allies=[]>], [#<Human:0x007fd0b3823760 @allies=[#<Human:0x007fd0b3823bc0 @allies=[...]>]>]
     #Human, do you have an alliance with me? Form alliance with human if we don't already have one.
-    unless human.has_alliance_with?(self)
-      human.form_alliance_with(self)
-    end
+    unless human.has_alliance_with?(self)  # => false, true
+      human.form_alliance_with(self)       # => nil
+    end                                    # => nil, nil
   end
 
   def has_alliance_with?(human)
-    @allies.include?(human)
+    @allies.include?(human)      # => false, true, true, true, false
   end
 
   def has_connection_with?(human)
@@ -28,14 +28,18 @@ class Human
 
 end
 
-rolando = Human.new
-purple_ray = Human.new([rolando])
-first_hated = Human.new
+rolando = Human.new                # => #<Human:0x007fd0b3823bc0 @allies=[]>
+purple_ray = Human.new([rolando])  # => #<Human:0x007fd0b3823760 @allies=[#<Human:0x007fd0b3823bc0 @allies=[#<Human:0x007fd0b3823760 ...>]>]>
+first_hated = Human.new            # => #<Human:0x007fd0b3821e88 @allies=[]>
 
-rolando.has_alliance_with?(purple_ray)
-purple_ray.has_alliance_with?(rolando)
-purple_ray.has_alliance_with?(first_hated)
+rolando.has_alliance_with?(purple_ray)      # => true
+purple_ray.has_alliance_with?(rolando)      # => true
+purple_ray.has_alliance_with?(first_hated)  # => false
 
-puts rolando.allies.count
-puts purple_ray.allies.count
-puts first_hated.allies.count
+puts rolando.allies.count      # => nil
+puts purple_ray.allies.count   # => nil
+puts first_hated.allies.count  # => nil
+
+# >> 1
+# >> 1
+# >> 0
---
+++ binary_search_tree.rb
@@ -0,0 +1,2 @@
+binary search tree and rebalancing the binary search tree is a tool for reconciliation.
+
