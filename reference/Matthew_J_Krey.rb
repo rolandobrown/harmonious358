3f962902b4f408361100d934a061a2c8e71c8a97
rolandobrown <rolando.brown@gmail.com>
2015-08-28 19:54:57 -0400

    Remember Matt

---
+++ reference/Matthew_J_Krey.rb
@@ -0,0 +1,3 @@
+"As I'm understading this, it's a set of emotions and beliefs that transcend cultural or religious boundaries." -"Matthe J Krey"
+
+"I don't have control over where I was born, but I was born with this brain, the evolutionary pinnacle of patterns matchings. So I experience this patterns, but my cultural context influences what I do with them" -"Matthe J Krey"
