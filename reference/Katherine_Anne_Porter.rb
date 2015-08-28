38a55959fe905f1d159e5aa8675e34e40db47d89
rolandobrown <rolando.brown@gmail.com>
2015-08-28 15:48:50 -0400

    add Katherine Anne Porter reference

---
+++ reference/Katherine_Anne_Porter.rb
@@ -0,0 +1,6 @@
+
+"INTRODUCTION: WHAT IS VERSION CONTROL?"
+"The past is never where you think you left it." — Katherine Anne Porter
+
+
+# Use as inspiration for telling the story of how Git explained her origin to Gideon. Note, Git was created as a result of Sagacious touching some unknown character that will be inspired by the life of  Linus Torvalds.
